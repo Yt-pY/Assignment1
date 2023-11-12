667ce9ae571fb8e8cf76ca3b2e2f632aec7967b2
lpy <lipeiyuan>
2023-11-12 21:28:18 +0800

    Initial Commit

---
+++ Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by lpy on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
